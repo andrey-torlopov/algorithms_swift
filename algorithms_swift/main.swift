00bcf374afff40b388a53904f309a2f95b08cb1a
Andrey Torlopov <torlopov.mail@ya.ru>
2024-01-19 10:26:27 +0300

    update

--- algorithms_swift/main.swift
+++ algorithms_swift/main.swift
@@ -1,5 +1,17 @@
 import Foundation
 
-let h = HappyNumber()
-print(h.isHappy(4))
+struct Foo {
+    let a: Int
+    let b: Bool
+}
 
+struct Foo2 {
+    let b: Bool
+    let a: Int
+}
+
+let size = MemoryLayout<Foo>.size
+let size2 = MemoryLayout<Foo2>.size
+
+print(size)
+print(size2)
