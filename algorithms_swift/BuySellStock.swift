47331137d3f7d5b0ee54eedebc778c4fa24e5e2c
Andrey Torlopov <torlopov.mail@ya.ru>
2024-01-22 12:13:22 +0300

    sell buy problem

---
+++ algorithms_swift/BuySellStock.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+class BuySellStockSolution {
+    func maxProfit(_ prices: [Int]) -> Int {
+        return 0
+    }
+
+    func test() {
+        print(maxProfit([7,1,5,3,6,4]), 5)
+    }
+}
